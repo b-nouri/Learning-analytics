0229231e622b3e4ec01b86cedac8a89596c30cac
Behzad Nouri <61915687+behnouri@users.noreply.github.com>
2021-02-16 23:32:21 +0100

    Update Main analysis.R

--- Main analysis.R
+++ Main analysis.R
@@ -6,6 +6,9 @@ library(visdat)
 library(stringr)
 library(ggplot2)
 library(lubridate)
+library(caret)
+library(doParallel)
+registerDoParallel(cores=4)
 
 #--------Load Data-------------------------------------------
 events <- read.csv("c:/ALCAPAS/Anon_events.dsv",
@@ -255,9 +258,11 @@ sap1 <- sap[sap$course_pk == 888132 &
 attempt_student1 <- attempt_student[attempt_student$course_pk == 888132 &
                     attempt_student$user_pk %in% sap1$user_pk,]
 
-event_course_week1 <- event_course_week[event_course_week$course_pk == 888132,]
+event_course_week1 <- event_course_week[event_course_week$course_pk == 888132 &
+                                          event_course_week$academic_week < 38,]
 
-event_week_day1 <- event_week_day[event_week_day$course_pk == 888132,]
+event_week_day1 <- event_week_day[event_week_day$course_pk == 888132 &
+                                    event_course_week$academic_week < 38,]
 
 event_course1 <- event_course[event_course$course_pk == 888132 &
                                 event_course$user_pk %in% sap1$user_pk,]
@@ -279,12 +284,356 @@ b <- attempt_student1 %>%
   group_by(user_pk) %>%
   spread(content_pk,highest) %>%
   group_by(user_pk) %>%
-  summarise_at(vars(c$content_pk), sum, na.rm = TRUE) %>%
-  ungroup()
+  summarise_at(vars(c$content_pk), sum, na.rm = TRUE)
+
+b <- b %>% rename_at(vars(c$content_pk), ~ paste0('test', 1:19))
+
+course1 <- merge(course1,b,by="user_pk",all.x = TRUE)
+course1 <- replace(course1, is.na(course1), 0)
+rm(b)
+rm(c)
+rm(df1)
+
+c <- unique(event_course_week1[,"academic_week"])
+c <- c %>%
+  arrange(academic_week)
+
+b <- event_course_week1 %>%
+  group_by(user_pk) %>%
+  spread(academic_week,n_event_week) %>%
+  group_by(user_pk) %>%
+  summarise_at(vars(as.factor(c$academic_week)), sum, na.rm = TRUE)
+
+b <- b %>% rename_at(vars(as.factor(c$academic_week)), ~ paste0('n_event_week', 1:nrow(c)))
+
+course1 <- merge(course1,b,by="user_pk",all.x = TRUE)
+course1 <- replace(course1, is.na(course1), 0)
+rm(b)
+rm(c)
+
+
+c <- unique(event_week_day1[,"academic_week"])
+c <- c %>%
+  arrange(academic_week)
+
+b <- event_week_day1 %>%
+  group_by(user_pk) %>%
+  spread(academic_week,n_day_week) %>%
+  group_by(user_pk) %>%
+  summarise_at(vars(as.factor(c$academic_week)), sum, na.rm = TRUE)
+
+b <- b %>% rename_at(vars(as.factor(c$academic_week)), ~ paste0('n_day_week', 1:nrow(c)))
+
+course1 <- merge(course1,b,by="user_pk",all.x = TRUE)
+course1 <- replace(course1, is.na(course1), 0)
+rm(b)
+rm(c)
+
+course1 <- subset(course1, select=-c(course_name))
+
+###--------Analysis - Forecasting------####
+###---Train Test Split----###
+course1 <- tibble::rowid_to_column(course1, "row")
+#course1 <- subset(course1, select=-c(n_event_course))
+set.seed(123)
+
+train1 <-sample_frac(course1, 0.8)
+sid<-train1$row
+test1 <- course1[-sid,]
+
+train_course1 <- train1[,3:ncol(train1)]
+
+id_test1 <- test1[,1:2]
+test_course1 <- test1[,4:ncol(test1)]
+
+##------Training and predicting----####
+
+train.control <- trainControl(method="LOOCV")
+
+train.control <- trainControl(method = "repeatedcv",
+                              number = 10, repeats = 3)
+
+
+model1_PR <- train(final_score ~ . , data = train_course1,
+                    method = "parRF", trControl = train.control,metric = "MAE",
+                   importance=T)
+
+model2_svm <- train(final_score ~ . , data = train_course1,
+                   method = "svmLinear", trControl = train.control,metric = "MAE")
+
+model3_glm <- train(final_score ~ . , data = train_course1,
+                         method = "glmStepAIC", trControl = train.control,metric = "MAE",
+                         preProcess = c("zv", "center", "scale", "pca"))
+
+
+model4_elasticnet <- train(final_score ~ . , data = train_course1,
+                         method = "glmnet", trControl = train.control,metric = "MAE",
+                         preProcess = c())
+
+model5_xgbTree <- train(final_score ~ . , data = train_course1,
+                        method = "xgbTree", trControl = train.control,metric = "MAE")
+
+
+model6_gbm <- train(final_score ~ . , data = train_course1,
+                    method = "gbm", trControl = train.control,metric = "MAE")
+
+print(min(model1_PR$results$MAE))
+print(min(model2_svm$results$MAE))
+print(min(model3_glm$results$MAE))
+print(min(model4_elasticnet$results$MAE))
+print(min(model5_xgbTree$results$MAE))
+print(min(model6_gbm$results$MAE))
+
+
+pred1 <- data.frame(predict(model1_PR, test_course1, type = "raw"))
+pred2 <- data.frame(predict(model2_svm, test_course1, type = "raw"))
+pred3 <- data.frame(predict(model3_glm, test_course1, type = "raw"))
+pred4 <- data.frame(predict(model4_elasticnet, test_course1, type = "raw"))
+pred5 <- data.frame(predict(model5_xgbTree, test_course1, type = "raw"))
+pred6 <- data.frame(predict(model6_gbm, test_course1, type = "raw"))
+
+# results <- cbind(id_test,test1$final_score)
+# names(results)[3] <- "actual_final_score"
+# 
+# results <- cbind(results,pred5)
+# names(results)[4] <- "prediction_xgbTree"
+
+results <- cbind(results,pred1,pred2,pred3,pred4,pred5,pred6)
+names(results)[4] <- "prediction_rf"
+names(results)[5] <- "prediction_svm"
+names(results)[6] <- "prediction_glm"
+names(results)[7] <- "prediction_glmnet"
+names(results)[8] <- "prediction_xgbTree"
+names(results)[9] <- "prediction_gbm"
+
+results1 <- results %>%
+  mutate(score_class = cut(as.numeric(results$actual_final_score), breaks=c(0,9.9,21), labels=c( 'pass', 'fail'))) %>%
+  mutate(class_rf = cut(as.numeric(results$prediction_rf), breaks=c(0,9.9,21), labels=c( 'pass', 'fail'))) %>%
+  mutate(class_svm = cut(as.numeric(results$prediction_svm), breaks=c(0,9.9,21), labels=c( 'pass', 'fail'))) %>%
+  mutate(class_glm = cut(as.numeric(results$prediction_glm), breaks=c(0,9.9,21), labels=c( 'pass', 'fail'))) %>%
+  mutate(class_glmnet = cut(as.numeric(results$prediction_glmnet), breaks=c(0,9.9,21), labels=c( 'pass', 'fail'))) %>%
+  mutate(class_xgbtree = cut(as.numeric(results$prediction_xgbTree), breaks=c(0,9.9,21), labels=c( 'pass', 'fail'))) %>%
+  mutate(class_gbm = cut(as.numeric(results$prediction_gbm), breaks=c(0,9.9,21), labels=c( 'pass', 'fail')))
+
+library(cvms)
+conf_matrm <- confusion_matrix(targets = results1$score_class,
+                 predictions = results1$class_rf)
+conf_matsvm <- confusion_matrix(targets = results1$score_class,
+                               predictions = results1$class_svm)
+conf_matglm <- confusion_matrix(targets = results1$score_class,
+                               predictions = results1$class_glm)
+conf_matglmnet <- confusion_matrix(targets = results1$score_class,
+                               predictions = results1$class_glmnet)
+conf_matxbtree <- confusion_matrix(targets = results1$score_class,
+                               predictions = results1$class_xgbtree)
+conf_matgbm <- confusion_matrix(targets = results1$score_class,
+                               predictions = results1$class_gbm)
+
+conf_matrm
+conf_matsvm
+conf_matglm
+conf_matglmnet
+conf_matxbtree
+conf_matgbm
+
+#rm(accuracy1)
+#accuracy1 <- as.data.frame(cbind("rf",mae_test_rf,conf_matrm$`Balanced Accuracy`,conf_matrm$F1))
+#names(accuracy1) <- c("algorithm","mae","accuracy","F1")
+accuracy <- data.frame(c("rf2",mae_test_rf,conf_matrm$`Balanced Accuracy`,conf_matrm$F1),
+                       c("svm2",mae_test_svm,conf_matsvm$`Balanced Accuracy`,conf_matsvm$F1),
+                       c("glm2",mae_test_glm,conf_matglm$`Balanced Accuracy`,conf_matglm$F1),
+                       c("glmnet2",mae_test_glmnet,conf_matglmnet$`Balanced Accuracy`,conf_matglmnet$F1),  
+                       c("xgbtree2",mae_test_xgbTree,conf_matxbtree$`Balanced Accuracy`,conf_matxbtree$F1),  
+                       c("gbm2",mae_test_gbm,conf_matgbm$`Balanced Accuracy`,conf_matgbm$F1))
+accuracy <- data.frame(t(accuracy))
+names(accuracy) <- c("algorithm","mae","accuracy","F1")
+
+accuracy1 <- rbind(accuracy1,accuracy)
+accuracy1 <- accuracy1[1:6,]
+mae_test_rf
+mae_test_svm
+mae_test_glm
+mae_test_glmnet
+mae_test_xgbTree
+mae_test_gbm
+
+results <- results %>%
+  mutate(score_rf =round(prediction_rf,0)) %>%
+  mutate(score_svm =round(prediction_svm,0)) %>%
+  mutate(score_glm =round(prediction_glm,0)) %>%
+  mutate(score_glmnet =round(prediction_glmnet,0)) %>%
+  mutate(score_xgbTree =round(prediction_xgbTree,0)) %>%
+  mutate(score_gbm =round(prediction_gbm,0))
+  
+mae_test_rf <- mean(abs(results$score_rf-results$actual_final_score))
+mae_test_svm <- mean(abs(results$score_svm-results$actual_final_score))
+mae_test_glm <- mean(abs(results$score_glm-results$actual_final_score))
+mae_test_glmnet <- mean(abs(results$score_glmnet-results$actual_final_score))
+mae_test_xgbTree <- mean(abs(results$score_xgbTree-results$actual_final_score))
+mae_test_gbm <- mean(abs(results$score_gbm-results$actual_final_score))
+
+mae_test_rf
+mae_test_svm
+mae_test_glm
+mae_test_glmnet
+mae_test_xgbTree
+mae_test_gbm
+
+gbmImp <- varImp(model2_svm, conditional=TRUE)
+gbmImp
+plot(gbmImp, top = 32)
+
+##------Training and predicting----####
+course1 <- tibble::rowid_to_column(course1, "row")
+set.seed(123)
+
+course1 <- course1 %>%
+  mutate(score_class = cut(as.numeric(course1$final_score), breaks=c(0,9.9,21), labels=c( 'pass', 'fail'))) %>%
+  mutate(score_class = as.factor(course1$score_class))
+
+train1 <-sample_frac(course1, 0.8)
+sid<-train1$row
+test1 <- course1[-sid,]
+
+train_course1 <- train1[,4:ncol(train1)]
+
+id_test1 <- test1[,1:2]
+test_course1 <- test1[,4:(ncol(test1)-1)]
+
+
+###-------------Classification------------###
+train.control <- trainControl(method="LOOCV",classProbs = TRUE)
+
+train.control <- trainControl(method = "repeatedcv",
+                              number = 10, repeats = 3,classProbs = TRUE)
+
+model1_PR <- train(score_class ~ . , data = train_course1,
+                   method = "parRF", trControl = train.control,metric="ROC")
+
+model2_svm <- train(score_class ~ . , data = train_course1,
+                    method = "svmLinear", trControl = train.control,metric="ROC")
+
+model3_glm <- train(score_class ~ . , data = train_course1,
+                    method = "glmStepAIC", trControl = train.control,metric="ROC",
+                    preProcess = c("zv", "center", "scale", "pca"))
+
+model4_elasticnet <- train(score_class ~ . , data = train_course1,
+                           method = "glmnet", trControl = train.control,metric="ROC",
+                           preProcess = c())
+
+model5_xgbTree <- train(score_class ~ . , data = train_course1,
+                        method = "xgbTree", trControl = train.control,metric="ROC")
+
+
+model6_gbm <- train(score_class ~ . , data = train_course1,
+                    method = "gbm", trControl = train.control,metric="ROC")
+
+model7 <- train(score_class ~ . , data = train_course1,
+                    method = "adaboost", trControl = train.control,metric="ROC")
+model8 <- train(score_class ~ . , data = train_course1,
+                    method = "AdaBoost.M1", trControl = train.control,metric="ROC")
+model9 <- train(score_class ~ . , data = train_course1,
+                    method = "amdai", trControl = train.control,metric="ROC")
+model10 <- train(score_class ~ . , data = train_course1,
+                    method = "AdaBag", trControl = train.control,metric="ROC")
+model11 <- train(score_class ~ . , data = train_course1,
+                    method = "bagFDAGCV", trControl = train.control,metric="ROC")
+model12 <- train(score_class ~ . , data = train_course1,
+                    method = "bagFDA", trControl = train.control,metric="ROC")
+model13 <- train(score_class ~ . , data = train_course1,
+                    method = "LogitBoost", trControl = train.control,metric="ROC")
+
+model30 <- train(score_class ~ . , data = train_course1,
+                    method = "svmLinearWeights", trControl = train.control,metric="ROC")
+
+model33 <- train(score_class ~ . , data = train_course1,
+                    method = "manb", trControl = train.control,metric="ROC")
+model34 <- train(score_class ~ . , data = train_course1,
+                    method = "mlpKerasDropoutCost", trControl = train.control,metric="ROC")
+model35 <- train(score_class ~ . , data = train_course1,
+                    method = "naive_bayes", trControl = train.control,metric="ROC")
+model35
+
+model39 <- train(score_class ~ . , data = train_course1,
+                 method = "pda", trControl = train.control,metric="ROC")
+model40 <- train(score_class ~ . , data = train_course1,
+                 method = "stepQDA", trControl = train.control,metric="ROC")
+model41 <- train(score_class ~ . , data = train_course1,
+                 method = "QdaCov", trControl = train.control,metric="ROC")
+model42 <- train(score_class ~ . , data = train_course1,
+                 method = "sparseLDA", trControl = train.control,metric="ROC")
+model43 <- train(score_class ~ . , data = train_course1,
+                 method = "snn", trControl = train.control,metric="ROC")
+model44 <- train(score_class ~ . , data = train_course1,
+                 method = "awtan", trControl = train.control,metric="ROC")
+model45 <- train(score_class ~ . , data = train_course1,
+                 method = "vbmpRadial", trControl = train.control,metric="ROC")
 
 
 
-df <- data.frame(month=rep(1:3,2),
-                 student=rep(c("Amy", "Bob"), each=3),
-                 A=c(9, 7, 6, 8, 6, 9),
-                 B=c(6, 7, 8, 5, 6, 7))
+
+
+print(max(model1_PR$results$Accuracy))
+print(max(model2_svm$results$Accuracy))
+print(max(model3_glm$results$Accuracy))
+print(max(model4_elasticnet$results$Accuracy))
+print(max(model5_xgbTree$results$Accuracy))
+print(max(model6_gbm$results$Accuracy))
+
+
+pred1 <- data.frame(predict(model1_PR, test_course1, type = "raw"))
+pred2 <- data.frame(predict(model2_svm, test_course1, type = "raw"))
+pred3 <- data.frame(predict(model3_glm, test_course1, type = "raw"))
+pred4 <- data.frame(predict(model4_elasticnet, test_course1, type = "raw"))
+pred5 <- data.frame(predict(model5_xgbTree, test_course1, type = "raw"))
+pred6 <- data.frame(predict(model6_gbm, test_course1, type = "raw"))
+
+
+
+results <- cbind(id_test,test1$final_score,test1$score_class)
+names(results)[3] <- "actual_final_score"
+names(results)[4] <- "actual_class"
+
+
+
+results <- cbind(results,pred1,pred2,pred3,pred4,pred5,pred6)
+names(results)[5] <- "prediction_rf"
+names(results)[6] <- "prediction_svm"
+names(results)[7] <- "prediction_glm"
+names(results)[8] <- "prediction_glmnet"
+names(results)[9] <- "prediction_xgbTree"
+names(results)[10] <- "prediction_gbm"
+
+
+conf_matrm <- confusion_matrix(targets = results$actual_class,
+                               predictions = results$prediction_rf)
+conf_matsvm <- confusion_matrix(targets = results$actual_class,
+                                predictions = results$prediction_svm)
+conf_matglm <- confusion_matrix(targets = results$actual_class,
+                                predictions = results$prediction_glm)
+conf_matglmnet <- confusion_matrix(targets = results$actual_class,
+                                   predictions = results$prediction_glmnet)
+conf_matxbtree <- confusion_matrix(targets = results$actual_class,
+                                   predictions = results$prediction_xgbTree)
+conf_matgbm <- confusion_matrix(targets = results$actual_class,
+                                predictions = results$prediction_gbm)
+
+conf_matrm
+conf_matsvm
+conf_matglm
+conf_matglmnet
+conf_matxbtree
+conf_matgbm
+
+#rm(accuracy1)
+#accuracy1 <- as.data.frame(cbind("rf",mae_test_rf,conf_matrm$`Balanced Accuracy`,conf_matrm$F1))
+#names(accuracy1) <- c("algorithm","mae","accuracy","F1")
+accuracy <- data.frame(c("rf2",conf_matrm$`Balanced Accuracy`,conf_matrm$F1),
+                       c("svm2",conf_matsvm$`Balanced Accuracy`,conf_matsvm$F1),
+                       c("glm2",conf_matglm$`Balanced Accuracy`,conf_matglm$F1),
+                       c("glmnet2",conf_matglmnet$`Balanced Accuracy`,conf_matglmnet$F1),  
+                       c("xgbtree2",conf_matxbtree$`Balanced Accuracy`,conf_matxbtree$F1),  
+                       c("gbm2",conf_matgbm$`Balanced Accuracy`,conf_matgbm$F1))
+accuracy <- data.frame(t(accuracy))
+names(accuracy) <- c("algorithm","accuracy","F1")
+
