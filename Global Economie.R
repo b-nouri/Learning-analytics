9000b5156795deac5e3380b68eb9b3f377a73c0b
Behzad Nouri <61915687+behnouri@users.noreply.github.com>
2021-05-01 16:57:26 +0200

    Revert "weeks added to globale economie"
    This reverts commit b9ac41858aae60b154edd8299e2619f363ea1d35.

--- Global Economie.R
+++ Global Economie.R
@@ -580,7 +580,6 @@ fviz_cluster(final2, data = df1)
 course1 <- course1 %>%
   mutate(cluster = final1$cluster)
 
-course <- course1
 ###---Train Test Split----###
 course1 <- subset(course1, select=-c(n_event_course))
 set.seed(123)
